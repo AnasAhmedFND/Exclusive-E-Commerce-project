667b0c02ec776a144c2f47347cc8ff2bd85a5e41
AnasAhmedFND <anasahmed4402@gmail.com>
2025-01-31 07:19:09 +0600

    header

---
+++ src/Components/Hedar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Hedar = () => {
+  return (
+    <div>
+      <div className=" flex border bg-[#000000] text-white gap-40 align-middle justify-center ">
+      <p className='text-center text-[#c2bebe] '>Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%! <span className='text-[#ffffff]'>ShopNow</span> </p>
+      <select className='text-[#c2bebe]' name="eng" id="eng">
+        <option className='text-[#171616]' value="English">English</option>
+        <option className='text-[#e413d2]' value="বাংলা">বাংলা</option>
+      </select>
+      </div>
+    </div>
+  )
+}
+
+export default Hedar
